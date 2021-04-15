28dfd164ee0c07e7f68bf156867e8783adf307c9
Karthikeyan R <Karthikn1499@gmail.com>
2021-04-15 22:26:14 +0530

    committing all the code for Student CRUD Application

---
+++ src/main/java/com/karthi/StudentCrud/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.karthi.StudentCrud.service;
+
+import java.util.List;
+
+import com.karthi.StudentCrud.model.Employee;
+
+public interface EmployeeService 
+{
+	List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
---
+++ src/test/java/com/karthi/StudentCrud/StudentCrudoperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthi.StudentCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudoperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
